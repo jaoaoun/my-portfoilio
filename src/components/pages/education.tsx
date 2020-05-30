040845baba7e250680ef687bfc5a67505bddfa45
jaoaoun <ntd_pst@hotmail.com>
2020-05-30 17:45:33 +0700

    Finish create edu page

--- src/components/pages/education.tsx
+++ src/components/pages/education.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import 'antd/dist/antd.css'
 import { Layout } from 'antd'
 import bgedu from '../../assets/bgedu.jpg'
-import imgedu1 from '../../assets/ungrad.png'
-
 import styled from 'styled-components'
 
 const { Content } = Layout
@@ -12,15 +10,36 @@ const education = () => {
   return (
     <BackgroundImg>
       <Content>
-        <Edu1>
-          <ImgEdu1 />
-
-          <TextEdu1>
-            King Mongkut's Institute of Technology Ladkrabang <br />
-            Bachelor's Degree in Information Engineering <br />
-            2016 - PRESENT
-          </TextEdu1>
-        </Edu1>
+        <DivImg1>
+          <Img1 src={require('../../assets/ungrad.png')} />
+        </DivImg1>
+        <TextEdu1>
+          King Mongkut's Institute of Technology Ladkrabang <br />
+          Bachelor's Degree in Information Engineering <br />
+          2016 - PRESENT
+        </TextEdu1>
+        <DivTimeline>
+          <ImgTim src={require('../../assets/timeline1.png')} />
+        </DivTimeline>
+        <DivImg>
+          <Img2 src={require('../../assets/highschool.png')} />
+        </DivImg>
+        <TextEdu2>
+          Assumption College Thonburi School <br />
+          Middle schooler & High schooler <br />
+          2009 - 2015
+        </TextEdu2>
+        <DivTimeline>
+          <ImgTim src={require('../../assets/timeline3.png')} />
+        </DivTimeline>
+        <DivImg>
+          <Img3 src={require('../../assets/kindergarten.png')} />
+        </DivImg>
+        <TextEdu3>
+          Sarasas Thonburi School <br />
+          Elementary schooler <br />
+          2014 - 2012
+        </TextEdu3>
       </Content>
     </BackgroundImg>
   )
@@ -29,23 +48,26 @@ const education = () => {
 export default education
 
 const BackgroundImg = styled(Layout)`
-  height: 100vh;
+  width: 100%;
+  /* height: 100vh; */
   background-image: url(${bgedu});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
 `
-const Edu1 = styled.div`
-  margin-top: 75px;
+const DivImg1 = styled.div`
+  margin-top: 60px;
+`
+const DivImg = styled.div`
+  margin-top: 15px;
+`
+
+const DivTimeline = styled.div`
+  margin-top: 15px;
 `
 
 const TextEdu1 = styled.p`
-  /* Educate-1 */
-  margin: auto;
-  top: 62px;
-
   /* King Mongkut's Institute of Technology Ladkrabang Bachelor's Degree in Information Engineering 2016 - PRESENT */
-
   font-family: 'Roboto', sans-serif;
   font-style: normal;
   font-weight: normal;
@@ -53,17 +75,70 @@ const TextEdu1 = styled.p`
   line-height: 140.62%;
   /* or 34px */
   text-align: center;
-
-  color: #000000;
+  -webkit-text-stroke: 0.05px #000000;
+  color: rgb(255, 255, 255);
+`
+const Img1 = styled.img`
+  /* ungrad 1 */
+  text-align: center;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 240px;
+`
+const Img2 = styled.img`
+  /* highschool 1 */
+  text-align: center;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 199px;
+`
+const Img3 = styled.img`
+  /* kindergarten 1 */
+  text-align: center;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 203px;
+`
+const ImgTim = styled.img`
+  /* timeline-1,timeline-3 */
+  text-align: center;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  width: 52px;
+  height: 303px;
 `
 
-const ImgEdu1 = styled.div`
-  /* ungrad 1 */
-  margin: auto;
-  width: 255px;
-  height: 240px;
-  top: 75px;
-  background-image: url(${imgedu1});
-  background-position: center;
-  background-repeat: no-repeat;
+const TextEdu2 = styled.p`
+  /* Assumption College Thonburi School Middle schooler & High schooler 2009 - 2015 */
+  margin-top: 15px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 140.62%;
+  /* or 34px */
+  text-align: center;
+  -webkit-text-stroke: 0.05px #000000;
+  color: rgb(255, 255, 255);
+`
+const TextEdu3 = styled.p`
+  /* Sarasas Thonburi School Elementary schooler 2014 - 2012 */
+  margin-top: 15px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 140.62%;
+  /* or 34px */
+  text-align: center;
+  -webkit-text-stroke: 0.05px #000000;
+  color: rgb(255, 255, 255);
 `
